2f43a89901fbc172c82a826fa60f498a7b41c20d
Igor Korobov <igor.korobov@dci-student.de>
2023-01-05 11:40:19 +0100

    temprorary commit

--- company/tests.py
+++ company/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.test import TestCase
+from rest_framework.test import APIClient
+from company.models import Company
+
+
+class TestCompanyAPIViewSet(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Company.objects.create(name='Nexflix', location='Mumbai', description='Movies')
+
+    def setUp(self):
+        self.client = APIClient()
+
+
+    # def test_if_company_created_returns_201_created(self, request):
+    #     data = {"name":"'Netflix'", "location":"Berlin", "description":"Movies"}
+    #     response = self.client.post('/api/v1/companies/', data)
+    #     self.assertEqual(response.status_code, 201)
+
+    def test_get_company_list(self):
+        response = self.client.get('/api/v1/companies/')
+        self.assertEqual(response.status_code, 200)
