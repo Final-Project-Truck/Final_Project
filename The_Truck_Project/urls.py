1dae8ef8840d56d4dccdc5ab92a04f5d2c2c4c4d
Fares <102142534+thprom@users.noreply.github.com>
2023-01-10 21:54:25 +0100

    Fully implement the registration process with forgetting password option, and authenticate the user login with the logout.

--- The_Truck_Project/urls.py
+++ The_Truck_Project/urls.py
@@ -51,7 +51,7 @@ urlpatterns = [
     path('register/', registerPage, name="register"),
     path('login/', loginPage, name="login"),
     path('logout/', logoutUser, name="logout"),
-    path('forget-password/', auth_views.PasswordResetView.as_view(template_name='forget_password.html'),name='forget_password'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'),name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),name='password_reset_done'),
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),name='password_reset_complete'),
