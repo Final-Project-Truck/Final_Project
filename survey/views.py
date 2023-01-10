6e11989e3219c756f3dd46e5906d41e42a23f60d
petertheprofessional <peterkelly3398@gmail.com>
2023-01-10 09:41:04 +0100

    added changes that i forgot in last commit

--- survey/views.py
+++ survey/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from baseuser.models import BaseUsers
+from company.models import Company
 from survey.models import Survey, Question, Option, Submission, AnswerChoice, AnswerText, SurveyQuestion
 from survey.serializers import SurveySerializer, QuestionSerializer, OptionSerializer, SubmissionSerializer, \
     AnswerChoiceSerializer, AnswerTextSerializer, SurveyQuestionSerializer
@@ -13,20 +15,64 @@ class SurveyAPIViewSet(ModelViewSet):
     queryset = Survey.objects.all()
     serializer_class = SurveySerializer
 
-    # def create(self, request, *args, **kwargs):
-    #     serializer = SurveySerializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     title = serializer.data['title']
-    #     created_at = serializer.data['created_at']
-    #     company = serializer.data['company']
-    #     creator = serializer.data['creator']
-    #
-    #     with transaction.atomic():
+    def create(self, request, *args, **kwargs):
+        serializer = SurveySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        title = serializer.data['title']
+        is_active = serializer.data['is_active']
+        creator = serializer.data['creator']
+        company = serializer.data['company']
+        created_at = serializer.data['created_at']
 
+        with transaction.atomic():
+            """Create Survey"""
+            survey_creator = BaseUsers.objects.get(id=creator)
+            survey_company = Company.objects.get(id=company)
+            survey = Survey.objects.create(title=title, is_active=is_active, creator=survey_creator,
+                                           company=survey_company, created_at=created_at)
+            survey.save()
+
+            """Template Question 1"""
+            template_question_1 = Question.objects.create(prompt="Question 1", type='cho', template_question=True)
+            template_question_1.save()
+            template_question_1_option_1 = Option.objects.create(question=template_question_1, text="True")
+            template_question_1_option_1.save()
+            template_question_1_option_2 = Option.objects.create(question=template_question_1, text="False")
+            template_question_1_option_2.save()
+            # related to template question 1
+
+            """Template Question 2"""
+            template_question_2 = Question.objects.create(prompt="Question 2", type='cho', template_question=True)
+            template_question_2.save()
+            template_question_2_option_1 = Option.objects.create(question=template_question_2, text="Strongly Agree")
+            template_question_2_option_1.save()
+            template_question_2_option_2 = Option.objects.create(question=template_question_2, text="Agree")
+            template_question_2_option_2.save()
+            template_question_2_option_3 = Option.objects.create(question=template_question_2, text="Neutral")
+            template_question_2_option_3.save()
+            template_question_2_option_4 = Option.objects.create(question=template_question_2, text="Disagree")
+            template_question_2_option_4.save()
+            template_question_2_option_5 = Option.objects.create(question=template_question_2, text="Strongly Disagree")
+            template_question_2_option_5.save()
+
+            """Template Question 3"""
+            template_question_3 = Question.objects.create(prompt="Question 3", type='txt', template_question=True)
+            template_question_3.save()
+
+            """Combine Template Questions with Company Survey"""
+            survey_question_1 = SurveyQuestion.objects.create(survey=survey, question=template_question_1)
+            survey_question_1.save()
+            survey_question_2 = SurveyQuestion.objects.create(survey=survey, question=template_question_2)
+            survey_question_2.save()
+            survey_question_3 = SurveyQuestion.objects.create(survey=survey, question=template_question_3)
+            survey_question_3.save()
+
+            return Response(SurveySerializer(survey).data, status=201)
 
 class QuestionAPIViewSet(ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+
 
 
 class SurveyQuestionAPIViewSet(ModelViewSet):
@@ -50,6 +96,7 @@ class SurveyQuestionAPIViewSet(ModelViewSet):
         else:
             SurveyQuestion.objects.create(survey_id=survey, question_id=question)
             return Response(serializer.data, status=201)
+
 
 class OptionAPIViewSet(ModelViewSet):
     queryset = Option.objects.all()
