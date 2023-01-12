8132fbe6714c3b1bae1af50a9180bb2c6accef9e
Igor Korobov <igor.korobov@dci-student.de>
2023-01-12 09:47:15 +0100

    changing logs 2

--- company/tests.py
+++ company/tests.py
@@ -9,19 +9,23 @@ class TestCompanyAPIViewSet(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        Company.objects.create(name='company_name', location='company_location', description='company_description')
+        Company.objects.create(name='company_name',
+                               location='company_location',
+                               description='company_description')
 
     def setUp(self):
         self.client = APIClient()
 
-
     def test_if_company_created_returns_201_created(self):
-        data = {"name":"'company_name'", "location": "company_location", "description":"company_description"}
+        data = {"name": "'company_name'", "location": "company_location",
+                "description": "company_description"}
         response = self.client.post('/api/v1/companies/', data)
         self.assertEqual(response.status_code, 201)
 
-    def test_if_survey_is_created_when_company_created_returns_True_if_exists(self):
-        data = {"name": "'company_name'", "location": "company_location", "description":"company_description"}
+    def test_if_survey_is_created_when_company_created_returns_True_if_exists(
+            self):
+        data = {"name": "'company_name'", "location": "company_location",
+                "description": "company_description"}
         response = self.client.post('/api/v1/companies/', data)
         output = get_object_or_404(Survey, title="'company_name'")
         self.assertNotEqual(output, 404)
@@ -32,7 +36,7 @@ class TestCompanyAPIViewSet(TestCase):
 
     def test_get_company_instance_returns_200_ok(self):
         response = self.client.get('/api/v1/companies/1/')
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
 
     def test_if_company_is_updated_returns_200_ok(self):
         response = self.client.get('/api/v1/companies/1/')
@@ -44,4 +48,3 @@ class TestCompanyAPIViewSet(TestCase):
         response = self.client.get('/api/v1/companies/1/')
         self.client.delete('/api/v1/companies/1/', response.data)
         self.assertEqual(response.status_code, 200)
-
