d095b8de2d6d7398f32c661babe0023d2c54609d
divya chandran <divya.chandran10@gmail.com>
2023-01-20 09:17:50 +0100

    Test case issues fixed and destroy implementation of survey

--- survey/admin.py
+++ survey/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from baseuser.models import BaseUsers
 from company.models import Company
+from survey.models import Survey
 
 # Register your models here.
 admin.site.register(Company)
 admin.site.register(BaseUsers)
+admin.site.register(Survey)
