4db7c834d50aaefa6f9ade5a9cfef3070fb96e0d
Fares <102142534+thprom@users.noreply.github.com>
2023-01-24 22:53:23 +0100

    fixing

--- The_Truck_Project/settings.py
+++ The_Truck_Project/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
